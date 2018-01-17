70d4840bb66a41f0a82d374b9e697daa78daf41c
Peter C Rigby <rigbypc@gmail.com>
2018-01-17 09:17:13 -0500

    The Sale class that we can't test due to dependencies on the Art56 register

---
+++ DependencyBreaking/src/point/of/sale/Sale.java
@@ -0,0 +1,14 @@
+package point.of.sale;
+
+public class Sale {
+
+	Art56 art56;
+	public Sale(Art56 art56) {
+		this.art56 = art56;
+	}
+	
+	public void scan(String barcode) {
+		art.display(barcode);
+	}
+
+}
