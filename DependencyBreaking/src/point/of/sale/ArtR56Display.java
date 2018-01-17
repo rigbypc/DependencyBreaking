ea0eb1d4febce62b5466e89bda9d97801da437ac
Peter C Rigby <rigbypc@gmail.com>
2018-01-17 15:14:05 -0500

    Move ArtR56 code into a new class that interacts with the physical register

---
+++ DependencyBreaking/src/point/of/sale/ArtR56Display.java
@@ -0,0 +1,15 @@
+package point.of.sale;
+
+public class ArtR56Display {
+
+	ArtR56 artR56;
+	public ArtR56Display(ArtR56 artR56) {
+		this.artR56 = artR56
+	}
+
+	public void showLine(String barcode) {
+		artR56.showLine(barcode);
+		
+	}
+
+}
--- DependencyBreaking/src/point/of/sale/Sale.java
+++ DependencyBreaking/src/point/of/sale/Sale.java
@@ -2,12 +2,13 @@ package point.of.sale;
 
 public class Sale {
 
-	PostgresCalls postgres;
-	ArtR56 artR56;
+	//PostgresCalls postgres;
+	ArtR56Display artR56;
 	
-	public Sale(ArtR56 artR56, PostgresCalls postgres) {
+	public Sale(ArtR56Display artR56) { 
+	//, PostgresCalls postgres) {
 		this.artR56 = artR56;
-		this.postgres = postgres;
+		//this.postgres = postgres;
 	}
 	
 	public void scan(String barcode) {
@@ -15,7 +16,7 @@ public class Sale {
 		artR56.showLine(barcode);
 		
 		//lookup barcode in postgres and get item
-		artR56.showLine(postgres.barcode(barcode));
+		//artR56.showLine(postgres.barcode(barcode));
 		
 	}
 
