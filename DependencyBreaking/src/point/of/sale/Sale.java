9c76d8a4bee511b171d72917ee8c90879626d997
Peter C Rigby <rigbypc@gmail.com>
2018-01-17 11:45:27 -0500

    Fix typo

--- DependencyBreaking/src/point/of/sale/Sale.java
+++ DependencyBreaking/src/point/of/sale/Sale.java
@@ -2,13 +2,13 @@ package point.of.sale;
 
 public class Sale {
 
-	Art56 art56;
-	public Sale(Art56 art56) {
-		this.art56 = art56;
+	ArtR56 artR56;
+	public Sale(ArtR56 artR56) {
+		this.artR56 = artR56;
 	}
 	
 	public void scan(String barcode) {
-		art.display(barcode);
+		artR56.display(barcode);
 	}
 
 }
