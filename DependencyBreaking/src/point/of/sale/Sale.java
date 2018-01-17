3917ae2882dc90a4c5ed7bdce430c5a9c3acf5e8
Peter C Rigby <rigbypc@gmail.com>
2018-01-17 11:52:27 -0500

    Add db component.

--- DependencyBreaking/src/point/of/sale/Sale.java
+++ DependencyBreaking/src/point/of/sale/Sale.java
@@ -2,13 +2,21 @@ package point.of.sale;
 
 public class Sale {
 
+	PostgresCalls postgres;
 	ArtR56 artR56;
-	public Sale(ArtR56 artR56) {
+	
+	public Sale(ArtR56 artR56, PostgresCalls postgres) {
 		this.artR56 = artR56;
+		this.postgres = postgres;
 	}
 	
 	public void scan(String barcode) {
+		//display the barcode
 		artR56.display(barcode);
+		
+		//lookup barcode in postgres and get item
+		artR56.display(postgres.barcode(barcode));
+		
 	}
 
 }
