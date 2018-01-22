7e0df9ca6271e58982b0a1a4614238b91e321437
Peter C Rigby <rigbypc@gmail.com>
2018-01-22 11:04:08 -0500

    Adding back the db

--- DependencyBreaking/src/point/of/sale/Sale.java
+++ DependencyBreaking/src/point/of/sale/Sale.java
@@ -2,13 +2,12 @@ package point.of.sale;
 
 public class Sale {
 
-	//PostgresCalls postgres;
+	PostgresCalls postgres;
 	Display display;
 	
-	public Sale(Display display) { 
-	//, PostgresCalls postgres) {
+	public Sale(Display display), PostgresCalls postgres) {
 		this.display = display;
-		//this.postgres = postgres;
+		this.postgres = postgres;
 	}
 	
 	public void scan(String barcode) {
@@ -16,7 +15,7 @@ public class Sale {
 		display.showLine(barcode);
 		
 		//lookup barcode in postgres and get item
-		//artR56.showLine(postgres.barcode(barcode));
+		artR56.showLine(postgres.barcode(barcode));
 		
 	}
 
